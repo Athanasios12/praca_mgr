86194216de1adc6ead80e1706758d08a631cf891
Athanasios12 <odys1234@gmail.com>
2017-05-18 23:09:05 +0200

    Finishing cpp11 concurency

---
+++ thesis/thesis_template/code_snippets/cpp_example_0.cpp
@@ -0,0 +1,53 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <utility>
+#include <chrono>
+#include <functional>
+#include <atomic>
+
+using namespace std;
+
+int protected_global = 0;
+std::mutex protected_global_mutex;  
+
+void test_fun(int n)
+{
+    for (int i = 0; i < n; ++i) {
+        std::cout << "Thread 1 executing\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+void safe_increment(int a, std::string &str)
+{
+    std::lock_guard<std::mutex> lock(protected_global_mutex);
+    protected_global += a;
+
+    cout << "Thread " << std::this_thread::get_id() << " incremented protected_global by " << a << '\n';
+	cout << str << endl;
+    // mutex is unlocked after lock_guard leaves current scope
+}
+
+int main()
+{
+    std::cout << "Init global : " << protected_global << '\n';
+	std::thread t1; //declaration, new thread was not created
+	//after giving execute function new thread starts running
+	std::thread t2(test_fun, 2);
+	std::thread t3(std::move(t2)); //calls move constructor
+	//t2 is not a thread anymore, t3 continues executing test_fun
+
+	//passing reference arguments, testing lock_guard use
+	std::string s1 = "Kamehameha!!";
+	std::string s2 = "Hadoken!!";
+    std::thread t4(safe_increment, 5, std::ref(s1));
+    std::thread t5(safe_increment, 10, std::ref(s2));
+	
+	//joinin with main thread
+    t3.join();
+	t4.join();
+	t5.join();
+
+    std::cout << "Final global : " << protected_global << '\n';
+}
--- thesis/thesis_template/document.lol
+++ thesis/thesis_template/document.lol
@@ -1,3 +1,5 @@
 \contentsline {lstlisting}{\numberline {3.1}Przyk\IeC {\l }ad tworzenia i uruchamiania w\IeC {\k a}tk\IeC {\'o}w}{9}{lstlisting.3.1}
 \contentsline {lstlisting}{\numberline {3.2}Przyk\IeC {\l }ad wykorzystanie muteksa do synchronizacji aplikacji wielow\IeC {\k a}tkowej}{10}{lstlisting.3.2}
 \contentsline {lstlisting}{\numberline {3.3}Prosta aplikacja wykorzystuj\IeC {\k a}ca dyrektyw\IeC {\k e} \texttt {\#pragma omp parallel} \cite {openmp_pacheco}}{12}{lstlisting.3.3}
+\contentsline {lstlisting}{\numberline {3.4}Podstawowe funkcjonalno\IeC {\'s}ci \texttt {std::thread} \cite {C++_ref}}{15}{lstlisting.3.4}
+\contentsline {lstlisting}{\numberline {3.5}Przyk\IeC {\l }ad zastosowania \texttt {std::future} i \texttt {std::async()} \cite {C++_ref}}{16}{lstlisting.3.5}
